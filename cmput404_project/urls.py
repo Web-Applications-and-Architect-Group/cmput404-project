84d8d0fecc57ca5a39a4b9139cae7063cd565dae
Tuxin Zhang <tuxinzhang@uws105037usr.uws.ualberta.ca>
2017-03-30 11:05:48 -0600

    added xie gang

--- cmput404_project/urls.py
+++ cmput404_project/urls.py
@@ -30,14 +30,14 @@ urlpatterns = [
     # """
     # Posts API (part 1)
     # """
-    url(r'^service/author/posts$',
+    url(r'^service/author/posts/$',
         api.All_Visible_Post_List_To_User.as_view(),
         name='authenticated_user_visible_post_list'),               # Allow [GET]. GET Done
 
     # """
     # Profile API
     # """
-    url(r'^service/author/(?P<author_id>[a-zA-Z0-9-_]+)$',          # Allow [GET]. GET partial Done TODO should contain friend list as well
+    url(r'^service/author/(?P<author_id>[a-zA-Z0-9-_]+)/$',          # Allow [GET]. GET partial Done TODO should contain friend list as well
         api.AuthorView.as_view(), name='author_profile'),
 
     # """
@@ -45,9 +45,9 @@ urlpatterns = [
     # """
     url(r'^service/posts/$',
         api.Public_Post_List.as_view(), name='public_post_list'),   # Allow [GET]. GET Done
-    url(r'^service/posts/(?P<post_id>[a-zA-Z0-9-_]+)$',
+    url(r'^service/posts/(?P<post_id>[a-zA-Z0-9-_]+)/$',
         api.Post_Detail.as_view(),name='a_single_post_detail'),     # Allow [GET, POST]. GET, POST Done
-    url(r'^service/author/(?P<author_id>[a-zA-Z0-9-_]+)/posts$',
+    url(r'^service/author/(?P<author_id>[a-zA-Z0-9-_]+)/posts/$',
         api.All_Visible_Post_List_From_An_Author_To_User.as_view(),
         name='authenticated_user_visible_post_list_from_an_author'),# Allow [GET]. GET Done
 
